f6fb74d6f27e9a20e4948558aa73e71cbdc53525
Бондарь Владислав Андреевич <Vl.Bondar@cft.ru>
2024-12-26 18:51:59 +0700

    Добавлен роутинг

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,21 @@
-import Counter from "./components/Counter"
+import { Route, Routes } from "react-router-dom";
 import './index.scss';
+import { Link } from "react-router-dom";
+import { AboutPageAsync } from "./pages/AboutPage/AboutPageAsync";
+import { MainPageAsync } from "./pages/MainPage/MainPageAsync";
+import { Suspense } from "react";
 
 const App = () => {
     return (
         <div className="app">
-            asdfef
-            <Counter />
+            <Link to={'/'}>Главная</Link>
+            <Link to={'/about'}>О сайте</Link>
+            <Suspense fallback={<div>Loading...</div>}>
+                <Routes>
+                    <Route path={'/about'} element={<AboutPageAsync />} />
+                    <Route path={'/'} element={<MainPageAsync />} />
+                </Routes>
+            </Suspense>
         </div>
     );
 };
