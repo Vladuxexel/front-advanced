68b8a1b94e8e6b1594200c94d3f19ac29b3e00ef
Бондарь Владислав Андреевич <Vl.Bondar@cft.ru>
2024-12-27 15:25:42 +0700

    Реализация FSD

---
+++ src/app/App.tsx
@@ -0,0 +1,20 @@
+import './styles/index.scss';
+import { Link } from 'react-router-dom';
+import { classNames } from 'shared/lib/classNames/classNames';
+import { useTheme } from './providers/ThemeProvider';
+import { AppRouter } from './providers/router';
+
+const App = () => {
+    const { theme, toggleTheme } = useTheme();
+
+    return (
+        <div className={classNames('app', {}, [theme])}>
+            <button onClick={toggleTheme}>Toggle theme</button>
+            <Link to={'/'}>Главная</Link>
+            <Link to={'/about'}>О сайте</Link>
+            <AppRouter />
+        </div>
+    );
+};
+
+export default App;
--- webpack.config.ts
+++ webpack.config.ts
@@ -7,14 +7,15 @@ export default (env: BuildEnv) => {
   const paths: BuildPaths = {
     entry: resolve(__dirname, 'src', 'index.tsx'),
     build: resolve(__dirname, 'build'),
-    html: resolve(__dirname, 'public', 'index.html')
+    html: resolve(__dirname, 'public', 'index.html'),
+    src: resolve(__dirname, 'src')
   }
-  
+
   const mode = env.mode || 'development';
   const PORT = env.port || 3000;
 
   const isDev = mode === 'development';
-  
+
   const config: Configuration = buildWebpackConfig({ mode, paths, isDev, port: PORT });
 
   return config;
