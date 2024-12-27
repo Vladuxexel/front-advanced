cb2a60c69f4585a2a0d10f818ecad33ca1bfbcf5
Бондарь Владислав Андреевич <Vl.Bondar@cft.ru>
2024-12-27 10:47:36 +0700

    Добавлен classNames

--- src/App.tsx
+++ src/App.tsx
@@ -5,12 +5,13 @@ import { AboutPageAsync } from "./pages/AboutPage/AboutPageAsync";
 import { MainPageAsync } from "./pages/MainPage/MainPageAsync";
 import { Suspense } from "react";
 import { useTheme } from "./theme/useTheme";
+import { classNames } from "./helpers/classNames/classNames";
 
 const App = () => {
     const {theme, toggleTheme} = useTheme();
 
     return (
-        <div className={`app ${theme}`}>
+        <div className={classNames('app', {}, [theme])}>
             <button onClick={toggleTheme}>Toggle theme</button>
             <Link to={'/'}>Главная</Link>
             <Link to={'/about'}>О сайте</Link>
