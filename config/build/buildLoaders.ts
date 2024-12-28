bfdf3d5e197cdf42ca38d541f14a362508c85252
Vladislav <vladuxexel@gmail.com>
2024-12-28 17:21:13 +0700

    ВЫнос переключателя темы и кнопки в shared-слой

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -1,36 +1,47 @@
-import { RuleSetRule } from "webpack";
+import { RuleSetRule } from 'webpack';
 import MiniCssExtractPlugin from 'mini-css-extract-plugin';
-import { BuildOptions } from "./types/config";
+import { BuildOptions } from './types/config';
 
 export function buildLoaders({ isDev }: BuildOptions): RuleSetRule[] {
+    const fileLoader = {
+        test: /\.(png|jpe?g|gif)$/i,
+        use: [
+            {
+                loader: 'file-loader',
+            },
+        ],
+    };
+
+    const svgLoader = {
+        test: /\.svg$/,
+        use: ['@svgr/webpack'],
+    };
+
     const stylesLoader = {
         test: /\.s[ac]ss$/i,
         use: [
-          // Creates `style` nodes from JS strings
-          isDev ? 'style-loader' : MiniCssExtractPlugin.loader,
-          // Translates CSS into CommonJS
-          {
-            loader: 'css-loader',
-            options: {
-                modules: {
-                    auto: (resPath: string) => Boolean(resPath.includes('.module.')),
-                    localIdentName: isDev ? '[path][name]__[local]--[hash:base64:8]' : '[hash:base64:8]'
-                }
-            }
-          },
-          // Compiles Sass to CSS
-          "sass-loader",
-        ]
-    }
-    
+            // Creates `style` nodes from JS strings
+            isDev ? 'style-loader' : MiniCssExtractPlugin.loader,
+            // Translates CSS into CommonJS
+            {
+                loader: 'css-loader',
+                options: {
+                    modules: {
+                        auto: (resPath: string) => Boolean(resPath.includes('.module.')),
+                        localIdentName: isDev ? '[path][name]__[local]--[hash:base64:8]' : '[hash:base64:8]',
+                    },
+                },
+            },
+            // Compiles Sass to CSS
+            'sass-loader',
+        ],
+    };
+
     const typescriptLoader = {
         test: /\.tsx?$/,
         use: 'ts-loader',
         exclude: /node_modules/,
     };
 
-    return [
-        typescriptLoader,
-        stylesLoader
-    ]
+    return [typescriptLoader, stylesLoader, svgLoader, fileLoader];
 }
