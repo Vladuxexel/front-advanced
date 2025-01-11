b27550e238d3ba469d04fcad1bc4c4319a18f008
Vladislav <vladuxexel@gmail.com>
2025-01-11 23:06:29 +0700

    Добавлена интернационализация

--- src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
+++ src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
@@ -1,7 +1,8 @@
 import { useState } from 'react';
 import { classNames } from 'shared/lib/classNames/classNames';
 import styles from './Sidebar.module.scss';
-import { ThemeSwitcher } from 'shared/ui/ThemeSwitcher';
+import { ThemeSwitcher } from 'widgets/ThemeSwitcher';
+import { LangSwitcher } from 'widgets/LangSwither';
 
 interface SidebarProps {
     className?: string;
@@ -19,6 +20,7 @@ export const Sidebar = ({ className }: SidebarProps) => {
             <button onClick={onToggle}>toggle</button>
             <div className={styles.switchers}>
                 <ThemeSwitcher />
+                <LangSwitcher className={styles.lang} />
             </div>
         </div>
     );
