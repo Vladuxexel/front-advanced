e03b3f08bbf13fb8507c8bb24a4dd467db75f3db
Vladislav <vladuxexel@gmail.com>
2025-01-23 23:06:24 +0700

    Разграничение dev-плагинов

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -6,7 +6,7 @@ import { BundleAnalyzerPlugin } from 'webpack-bundle-analyzer';
 import { BuildOptions } from './types/config';
 
 export function buildPlugins({ paths, isDev }: BuildOptions): WebpackPluginInstance[] {
-    return [
+    const plugins = [
         new HtmlWebpackPlugin({
             template: paths.html,
         }),
@@ -18,10 +18,17 @@ export function buildPlugins({ paths, isDev }: BuildOptions): WebpackPluginInsta
         new DefinePlugin({
             __IS_DEV__: String(isDev),
         }),
-        new HotModuleReplacementPlugin(),
         new ReactRefreshPlugin(),
-        new BundleAnalyzerPlugin({
-            openAnalyzer: false
-        })
     ];
+
+    if (isDev) {
+        plugins.push(new HotModuleReplacementPlugin());
+        plugins.push(
+            new BundleAnalyzerPlugin({
+                openAnalyzer: false,
+            })
+        );
+    }
+
+    return plugins;
 }
