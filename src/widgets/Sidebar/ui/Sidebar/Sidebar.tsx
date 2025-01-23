81a861e020c7d6943675e8f024bfc4bcd912ff7c
Vladislav <vladuxexel@gmail.com>
2025-01-23 23:12:28 +0700

    Фикс кнопки сайдбара

--- src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
+++ src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
@@ -3,6 +3,7 @@ import { classNames } from 'shared/lib/classNames/classNames';
 import { ThemeSwitcher } from 'widgets/ThemeSwitcher';
 import { LangSwitcher } from 'widgets/LangSwither';
 import { Button } from 'shared/ui/Button/Button';
+import { useTranslation } from 'react-i18next';
 import styles from './Sidebar.module.scss';
 
 interface SidebarProps {
@@ -11,6 +12,7 @@ interface SidebarProps {
 
 export const Sidebar = ({ className }: SidebarProps) => {
     const [collapsed, setCollapsed] = useState(false);
+    const { t } = useTranslation();
 
     const onToggle = () => {
         setCollapsed((value) => !value);
@@ -22,7 +24,7 @@ export const Sidebar = ({ className }: SidebarProps) => {
             className={classNames(styles.sidebar, { [styles.collapsed]: collapsed }, [className])}
         >
             <Button data-testid='sidebar-toggle' onClick={onToggle}>
-                toggle
+                {t('Переключить')}
             </Button>
             <div className={styles.switchers}>
                 <ThemeSwitcher />
